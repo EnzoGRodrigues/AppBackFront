b26e29f0799334ed2f6acbb139e4fe98c0ca2443
Enzo Rodrigues <enzorodrigues272@icloud.com>
2023-10-25 22:59:49 -0300

    criando os elementos na HomeScreen.

--- AppBackFront/Feature/HomeVC/Screen/HomeScreen.swift
+++ AppBackFront/Feature/HomeVC/Screen/HomeScreen.swift
@@ -24,7 +24,7 @@ class HomeScreen: UIView {
     }()
     
     lazy var searchBar: UISearchBar = {
-       let searchBar = UISearchBar()
+        let searchBar = UISearchBar()
         searchBar.translatesAutoresizingMaskIntoConstraints = false
         searchBar.clipsToBounds = true
         searchBar.layer.cornerRadius = 20
@@ -33,11 +33,11 @@ class HomeScreen: UIView {
     }()
     
     lazy var collectionView : UICollectionView = {
-       let collectionView = UICollectionView()
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewLayout.init())
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
-         // TO DO: Register - chamar a classe da célula que vai popular a collectionView
+        // TO DO: Register - chamar a classe da célula que vai popular a collectionView
         let layout = UICollectionViewFlowLayout()
         layout.sectionInset = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24) //serve para dizer que quando a collectionView nascer, a célula vai conter 24 de distancia no lado esquerdo e direito.
         layout.scrollDirection = .horizontal
@@ -46,17 +46,18 @@ class HomeScreen: UIView {
     }()
     
     lazy var tableView: UITableView = {
-       let tableView = UITableView()
+        let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         // TO DO: REGISTER - chamar a classe da célula e registrar ela aqui
         tableView.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
         tableView.separatorStyle = .none
         return tableView
     }()
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         addViews()
+        configConstrains()
     }
     
     required init?(coder: NSCoder) {
@@ -69,6 +70,35 @@ class HomeScreen: UIView {
         viewBackground.addSubview(searchBar)
         viewBackground.addSubview(collectionView)
         addSubview(tableView)
+    }
+    
+    func configConstrains(){
+        NSLayoutConstraint.activate([
+            viewBackground.topAnchor.constraint(equalTo: topAnchor),
+            viewBackground.leadingAnchor.constraint(equalTo: leadingAnchor),
+            viewBackground.trailingAnchor.constraint(equalTo: trailingAnchor),
+            viewBackground.heightAnchor.constraint(equalToConstant: 220),
+            
+            logoImageView.topAnchor.constraint(equalTo: viewBackground.topAnchor, constant: 45),
+            logoImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+            logoImageView.heightAnchor.constraint(equalToConstant: 40),
+            logoImageView.widthAnchor.constraint(equalToConstant: 40),
+            
+            searchBar.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 10),
+            searchBar.leadingAnchor.constraint(equalTo: logoImageView.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+            searchBar.heightAnchor.constraint(equalToConstant: 46),
+            
+            collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 10),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: viewBackground.bottomAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: viewBackground.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
     }
     
 }
