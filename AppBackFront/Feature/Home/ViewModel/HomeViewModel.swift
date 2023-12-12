60090b173e42e40ad941d7c85e7505e7f762dfa8
Enzo Rodrigues <enzorodrigues272@icloud.com>
2023-12-12 19:33:25 -0300

    finalizando filtros da Home

--- AppBackFront/Feature/Home/ViewModel/HomeViewModel.swift
+++ AppBackFront/Feature/Home/ViewModel/HomeViewModel.swift
@@ -82,14 +82,43 @@ class HomeViewModel {
     
     // MARK: - Filter
     
+    var typeFilter: Int? {
+        return searchNftData?.filterListNft?.first(where: {$0.isSelected == true})?.id
+    }
+    
     public func filterSearchText(_ text: String){ //método para usar na pesquisa de texto.
+        
+        var nftList: [Nft] = []
+        //criando filtro para que possa pegar todos os types iguais e exibir para mim na tela depois de eu clicar no filtro.
+        if typeFilter == 0 { //se caso o meu type for zero
+            nftList = nftData?.nftList ?? [] // me retorna a minha lista completa
+        }else {
+            nftList = nftData?.nftList?.filter({$0.type == typeFilter ?? 0}) ?? [] // caso contrário, me restorne o filtro com o tipo que eu selecionei
+        }
+        
+        //criando filtro que depois de clicado e separado todos os types iguais, eu possa filtrar, nessa nova lista criada, pelo que esrá escrito.
         if text.isEmpty {
-            searchNftData?.nftList = nftData?.nftList
+            searchNftData?.nftList = nftList
         }else{
-            searchNftData?.nftList = nftData?.nftList?.filter({ nft in //to pensando o nftData.nftList e fazendo um filtro dele. Onde eu digo que de acordo com o nome do usuário, ou seja, olha para o nome do usuário e vê se o texto que contem nele (.contains) é igual ao texto que eu digitei pra você (o text passado no parametro)
+            searchNftData?.nftList = nftList.filter({ nft in //to pensando o nftData.nftList e fazendo um filtro dele. Onde eu digo que de acordo com o nome do usuário, ou seja, olha para o nome do usuário e vê se o texto que contem nele (.contains) é igual ao texto que eu digitei pra você (o text passado no parametro)
                 //o retorno dessa pesquisa será atribuida ao meu searchNftData. por isso eu tenho que alterar para searchNftData em todas as outras partes do código acima, porque quando voltar no meu numberOfRowsInsection por exemplo, ele terá que me dar todas as linhas da minha pesquisa e não todas as linhas de tudo o que eu tenho. o nftData é TUDO o que eu tenho. O searchNftData é o resultado do filtro, da pesquisa q o usuário fez em cima de tudo o que eu tenho, em cima do nftData.
                 return nft.userName?.lowercased().contains(text.lowercased()) ?? false
             })
         }
+    }
+    
+    public func setFilter(indexPath: IndexPath, searchText: String){
+        var filterNft: [FilterNft] = []
+        for (index, value) in (searchNftData?.filterListNft ?? []).enumerated(){
+            var type = value //crio essa variável porque o nosso value ele é o filterNft completo. e o meu value ele é por padrão do tipo let, então não pode ser alterado, por isso crio uma variável que recebe o value para poder acessar o isSelect e alterar o valor dele.
+            if index == indexPath.row{ //se a posição que eu selecionei é a mesma posição que o for está percorrendo, então ele irá transformar o "isSelected" em true e irá selecionar o filtro correto.
+                type.isSelected = true
+            }else{
+                type.isSelected = false
+            }
+            filterNft.append(type)
+        }
+        searchNftData?.filterListNft = filterNft
+        filterSearchText(searchText)
     }
 }
