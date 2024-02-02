68a03b67e37dfb89e6d718d730cce746a207abc1
Enzo Rodrigues <enzorodrigues272@icloud.com>
2024-02-01 23:08:42 -0300

    criacao da tela de detalhe

---
+++ AppBackFront/Feature/NftDetail/ViewModel/NftDetailViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  NftDetailViewModel.swift
+//  AppBackFront
+//
+//  Created by Enzo Rodrigues on 12/12/23.
+//
+
+import UIKit
+
+class NftDetailViewModel {
+    
+    let nft: Nft
+    
+    init(nft: Nft){
+        self.nft = nft
+    }
+
+}
